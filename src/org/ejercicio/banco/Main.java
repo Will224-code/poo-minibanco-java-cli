7ccff1a76dfcdac09ae6032719c06381a43b76b4
William Enrique Caamal Caamal <a24216894@alumnos.uady.mx>
2025-09-02 18:32:09 +0000

    otro

--- src/org/ejercicio/banco/Main.java
+++ src/org/ejercicio/banco/Main.java
@@ -5,6 +5,8 @@ public class Main{
         Cliente clientel = new Cliente("Luis Gomez", 25672946);
         CuentaBancaria cuental = new CuentaBancaria(693531,clientel,10000);
 
-    
+        CuentaBancaria cuenta2 = new CuentaBancaria(693531, new Cliente("Luis Gomi", 25672946)  ,10000);
+
+        new Cliente("Madre", 2446);
     }
 }
